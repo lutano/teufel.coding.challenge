6692b9f8e4d65bd842503a7cb6508cb66abb56d1
Luciano Spilotros <luciano.spilotros@gmail.com>
2024-10-31 12:16:10 +0100

    Add cross browser compatibility

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -17,6 +17,7 @@ export const i18n = createI18n({
   },
 });
 
+// Add locale management functionalities
 export function useLocale() {
   const setLocale = async (locale: SupportedLocale) => {
     (i18n.global.locale.value as SupportedLocale) = locale;
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import autoprefixer from "autoprefixer";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
-})
+  css: {
+    postcss: {
+      plugins: [
+        autoprefixer({
+          cascade: true,
+          add: true,
+          remove: true,
+        }),
+      ],
+    },
+  },
+});
